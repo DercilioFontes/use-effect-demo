91a4a2da4075629a331b29db7a38159eecc547f3
DercilioFontes <dercilioafontes@gmail.com>
2024-01-06 11:55:15 -0500

    Finished Demo1 and initial Demo2

--- src/components/Demo1.tsx
+++ src/components/Demo1.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 
+/*
+ > Demo1
+  - Pure function
+  - Reactive
+  - Profiler (Dev tool)
+  - Events
+ */
+
 export const Demo1 = () => {
-  const [count, setCount] = useState(0);
+  const a = 0;
+  const [b, setB] = useState(0);
 
   return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+    <>
+      <div className="card">
+        <button onClick={() => a + 1}>count A: {a}</button>
+      </div>
+      <div className="card">
+        <button onClick={() => setB(b + 1)}>count B: {b}</button>
+      </div>
+    </>
   );
 };
