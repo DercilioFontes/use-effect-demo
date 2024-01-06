3876fca01be1cc85528e3cb92022254a3b87c5a8
DercilioFontes <dercilioafontes@gmail.com>
2024-01-06 11:34:31 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import { Demo1 } from "./components/Demo1";
 
 function App() {
-  return <Demo1 />;
+  return (
+    <>
+      <Demo1 />
+    </>
+  );
 }
 
 export default App;
